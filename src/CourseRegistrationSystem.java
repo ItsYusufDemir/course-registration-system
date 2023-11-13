64996a2014000de63bfd7ee88b43df18008e631c
Yusuf Demir <jamesyusuf36@gmail.com>
2023-11-13 23:24:31 +0300

    main class and CourseRegistrationSystem class are implemented

---
+++ src/CourseRegistrationSystem.java
@@ -0,0 +1,19 @@
+public class CourseRegistrationSystem {
+    
+    void start() {
+        User user;
+
+        while(true) {
+
+            //Create commond line interface for login page and start it
+            CLILogin login = new CLILogin();
+            user = login.loginPage();
+            
+            
+            //Create commond line interface for first menu page and start it
+            user.getMyPage(); 
+
+        }
+    }
+
+}
