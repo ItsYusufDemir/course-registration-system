ad56b25dddd8c5bf3c72dcd7ddf3af77ecf85729
beyz4n <95589101+beyz4n@users.noreply.github.com>
2023-11-14 23:51:53 +0300

    I added the classes for Student and some of the course related ones.

---
+++ src/SelectedCourse.java
@@ -0,0 +1,28 @@
+public class SelectedCourse {
+    private Student student;
+    private Course course;
+    private CourseStatus status;
+
+    public SelectedCourse(Student student, Course course) {
+        this.student = student;
+        this.course = course;
+        this.status = CourseStatus.Draft;
+    }
+
+    public CourseStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(CourseStatus status) {
+        this.status = status;
+    }
+
+    public boolean isApproved() {
+
+        if (status == CourseStatus.Approved) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
