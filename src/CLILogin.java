0b4b39632cb947337e5f2b2dc51118fd1a209495
selnaydinn <selinnaydin1234@gmail.com>
2023-11-14 23:22:33 +0300

    CLIAdvisor, CLILogin, SelectedCourse are implemented

---
+++ src/CLILogin.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+import java.util.Stack;
+
+public class CLILogin {
+    public User loginPage(){
+
+        do {
+            System.out.println(" Login ");
+            System.out.println(" ******** ");
+            Scanner input = new Scanner(System.in);
+
+            System.out.print("Username:");
+            String username = input.nextLine();
+
+            System.out.print("Password:");
+            String password = input.nextLine();
+
+            UserController userController = new UserController();
+            User user = userController.authenticateUser(username, password);
+
+            if(user == null){
+                System.out.println("Invalid username or password.");
+            }
+        }
+      //recursive yapma, do while a dönüştür
+        while(user == null );
+
+        return user;
+        }
+
+    }
+
