d330b3a497a6797aaa96d90be7f8c51d22fe2415
Despance <despance1927@hotmail.com>
2023-11-14 20:13:51 +0300

    user controller deleted

--- src/model/User.java
+++ src/model/User.java
@@ -4,25 +4,60 @@ import interfaces.Showable;
 
 public abstract class User implements Showable {
     private int userId;
-    private String username;
-    private String password;
     private String firstName;
     private String lastName;
     private boolean status;
 
-    public User(int userId, String username, String password, String firstName, String lastName, boolean status) {
+    public User(int userId, String firstName, String lastName, boolean status) {
         this.userId = userId;
-        this.username = username;
-        this.password = password;
         this.firstName = firstName;
         this.lastName = lastName;
         this.status = status;
     }
 
-    private User() {
+    public User(String username, String password) {
+        AuthenticateService authenticateService = new AuthenticateService();
+        User user = authenticateService.authenticateUser(username, password);
+        this.userId = user.getUserId();
+        this.firstName = user.getFirstName();
+        this.lastName = user.getLastName();
+        this.status = user.isStatus();
     }
 
     public void logout() {
-        throw new UnsupportedOperationException("Not supported yet.");
+        System.out.println("User logged out");
+        return;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
     }
 }
