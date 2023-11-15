3dd5028596f9af0fd8d242e98410b677b52ffa9c
Despance <despance1927@hotmail.com>
2023-11-15 18:01:09 +0300

    usercontroller added

---
+++ src/model/UserController.java
@@ -0,0 +1,29 @@
+package model;
+
+public class UserController {
+
+    private User currentUser;
+
+    public UserController() {
+        currentUser = null;
+    }
+
+    User loginUser(String username, String password) {
+        AuthenticateService authenticateService = new AuthenticateService();
+        return currentUser = authenticateService.authenticateUser(username, password);
+    }
+
+    void logoutUser() {
+        currentUser.logout();
+        currentUser = null;
+    }
+
+    User getCurrentUser() {
+        return currentUser;
+    }
+
+    void setCurrentUser(User currentUser) {
+        this.currentUser = currentUser;
+    }
+
+}
