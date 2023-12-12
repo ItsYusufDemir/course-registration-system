72ca839ec9ab0fc5eaaed2bf4b09535ef661dc92
beyz4n <95589101+beyz4n@users.noreply.github.com>
2023-12-12 16:41:09 +0300

    edited due to recent changes

--- iteration2/src/controllers/AdminController.java
+++ iteration2/src/controllers/AdminController.java
@@ -12,31 +12,20 @@ public class AdminController {
     public AdminController() {
 
     }
+
     public AdminController(Admin currentAdmin) {
         this.currentAdmin = currentAdmin;
-    }
-
-    public List<Student> getStudentList(){
-        return currentAdmin.getStudentList();
-    }
-
-    public List<Advisor> getAdvisorList(){
-        return currentAdmin.getAdvisorList();
     }
 
     public List<Course> getCourseList(){
         return currentAdmin.getCourseList();
     }
 
-    public Student createStudentList(){
-        return currentAdmin.createStudentList();
+    public Constraint getConstraint(){
+        return currentAdmin.getConstraint();
     }
 
-    public Advisor createAdvisorList(){
-        return currentAdmin.createAdvisorList();
-    }
-
-    public Course createCourseList(){
+    public Course createCourse(Course course){
         return currentAdmin.createCourseList();
     }
 
@@ -44,27 +33,7 @@ public class AdminController {
         currentAdmin.editConstraint(editedAttributes);
     }
 
-    public Student editStudent(Student student,HashMap<Integer, String> editedAttributes){
-        return currentAdmin.editStudent(student, editedAttributes);
-    }
-
-    public Advisor editAdvisor(Advisor advisor,HashMap<Integer, String> editedAttributes){
-        return currentAdmin.editAdvisor(advisor, editedAttributes);
-    }
-    public Course editCourse(Course course,HashMap<Integer, String> editedAttributes){
-        return currentAdmin.editCourse(course, editedAttributes);
-    }
-
-
-    public boolean deleteStudentList(Student student){
-        return currentAdmin.deleteStudentList();
-    }
-
-    public boolean deleteAdvisorList(Advisor advisor){
-        return currentAdmin.deleteAdvisorList();
-    }
-
-    public boolean createCourseList(Course course){
-        return currentAdmin.deleteCourseList();
+    public boolean deleteCourse(Course course){
+        return currentAdmin.deleteCourse(course);
     }
 }
