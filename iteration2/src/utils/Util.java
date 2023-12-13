86409ec526306bcd0bb229e38440cc8213fe5d60
Sena Ektiricioğlu <senaektiricioglu@gmail.com>
2023-12-13 19:19:05 +0300

    Edit for createNewCoursePage(): section time and date returned to list

--- iteration2/src/utils/Util.java
+++ iteration2/src/utils/Util.java
@@ -2,6 +2,9 @@ package iteration2.src.utils;
 
 import iteration2.src.enums.Color;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Util {
     public static void clearScreen() {
         System.out.print("\033[H\033[2J");
@@ -69,6 +72,59 @@ public class Util {
         int rowNumberInt = Integer.parseInt(getRowNumberFromInput(rowNumber));
         if (rowNumberInt > list.length || rowNumberInt < 1) {
             return false;
+        }
+        return true;
+    }
+
+    public static ArrayList<String> makeArrayList(String splitPattern, String textToSplit){
+        String[] splitArray = textToSplit.split(splitPattern);
+        ArrayList<String> newlist = new ArrayList<String>(Arrays.asList(splitArray));
+        for(int i = 0; i < newlist.size(); i++){
+            newlist.set(i, newlist.get(i).trim());
+        }
+        return newlist;
+    }
+
+    public static boolean isInputFormatTrueForDay(ArrayList<String> list){
+        int length = list.size();
+        for(int i = 0; i < length; i++){
+            if(!("Monday".equalsIgnoreCase(list.get(i)) ||
+                    "Tuesday".equalsIgnoreCase(list.get(i)) ||
+                    "Wednesday".equalsIgnoreCase(list.get(i)) ||
+                    "Thursday".equalsIgnoreCase(list.get(i)) ||
+                    "Friday".equalsIgnoreCase(list.get(i)))){
+                return false;
+            }
+        }
+        return true;
+    }
+    public static boolean isInputFormatTrueForTime(ArrayList<String> list){
+        int length = list.size();
+        int hour;
+        int minute;
+        String[] timeList;
+        for(int i = 0; i < length; i++){
+            if(!list.get(i).contains(":")){
+                return false;
+            }
+
+            timeList = list.get(i).split(":");
+            try{
+                hour = Integer.parseInt(timeList[0]);
+                minute = Integer.parseInt(timeList[0]);
+            }catch (Exception e){
+                return false;
+            }
+
+            if(0 < hour && hour > 23){
+                return false;
+            }
+
+
+            if(0 < minute && minute > 59){
+                return false;
+            }
+
         }
         return true;
     }
