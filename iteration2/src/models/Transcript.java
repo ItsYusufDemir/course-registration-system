ed2ccb13d5390a91eddba5c4de22956d7a60a9f5
Yusuf Demir <jamesyusuf36@gmail.com>
2023-12-14 21:50:52 +0300

    engineering project and repeated courses are considered

--- iteration2/src/models/Transcript.java
+++ iteration2/src/models/Transcript.java
@@ -1,6 +1,10 @@
 package iteration2.src.models;
 import iteration2.src.enums.CourseResult;
+import iteration2.src.enums.CourseType;
+
 import java.util.*;
+
+import iteration2.src.utils.DatabaseManager;
 
 public class Transcript{
 
@@ -56,6 +60,45 @@ public class Transcript{
 
     public void setTakenCourses(List<CourseGrade> takenCourses) {
         this.takenCourses = takenCourses;
+    }
+
+    
+    //This method checks if the student has taken all the courses that are required for graduation
+    //Which is at least 165 credits exluding nte, te, ue, fe and isg
+    public boolean checkEngineeringProjectAvailability() {
+        HashMap<Integer,String> constraint = DatabaseManager.getInstance().getConstraints();
+
+        double totalCreditsForEngineeringProject = 0;
+
+        //Those course types are not count for engineering project
+        CourseType restrictedCourseTypes[] = {
+            CourseType.NONTECHNICAL_ELECTIVE,
+            CourseType.UNIVERSITY_ELECTIVE,
+            CourseType.TECHNICAL_ELECTIVE,
+            CourseType.FACULTY_ELECTIVE,
+        };
+
+        //Those courses are not count for engineering project
+        String restrictedCourses[] = {"ISG121", "ISG122"};
+        
+
+        for(int i = 0; i < takenCourses.size(); i++){
+
+            if(takenCourses.get(i).getCourseResult() == CourseResult.PASSED && 
+                Arrays.asList(restrictedCourseTypes).contains(takenCourses.get(i).getCourse().getCourseType()) == false &&
+                Arrays.asList(restrictedCourses).contains(takenCourses.get(i).getCourse().getCourseCode()) == false) {
+
+
+                totalCreditsForEngineeringProject += takenCourses.get(i).getCourse().getCourseCredit();
+
+            }
+        }
+
+        if (totalCreditsForEngineeringProject >= Integer.parseInt(constraint.get(3))) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
     
