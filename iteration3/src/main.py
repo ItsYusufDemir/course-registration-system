18508383cacc8f46396969f1ae5617b0c4f0bea0
Yusuf Demir <jamesyusuf36@gmail.com>
2023-12-30 17:56:08 +0300

    Main and CourseRegistrationSystem are implemented

--- iteration3/src/main.py
+++ iteration3/src/main.py
@@ -6,9 +6,16 @@ from dotenv import load_dotenv
 load_dotenv()
 sys.path.insert(0, os.getenv("PYTHONPATH"))
 
-from iteration3.src.utils.DatabaseManager import DatabaseManager
+from iteration3.src.models.CourseRegistrationSystem import CourseRegistrationSystem
+from iteration3.src.utils.Util import Util
 
-print(DatabaseManager.getInstance().constraintList[0].minRequiredECTSForTermProject)
-DatabaseManager.getInstance().constraintList[0].minRequiredECTSForTermProject = 32
-DatabaseManager.getInstance().saveConstraintsList()
-print("saved to db")
+class Main:
+    @staticmethod
+    def main():
+        # Create the course registration system and start it
+        courseRegistrationSystem = CourseRegistrationSystem()
+        Util.getLogger().info("Course registration system starting.")
+        courseRegistrationSystem.start()
+
+if __name__ == "__main__":
+    Main.main()
